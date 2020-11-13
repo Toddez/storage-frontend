e1d88f5aae21986b346208c16acfff3d99d7ec61
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-13 21:04:40 +0100

    Added theme to config

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,3 +1,19 @@
 const api_url = process.env.API_URL || 'http://localhost:1337';
 
-export { api_url };
+const theme = {
+    lineNumberColor: '#848484',
+    lineNumberBgColor: '#ff0000',
+    backgroundColor: '#212121',
+    textColor: '#eceff1',
+    keywordColor: '#C792EA',
+    builtInColor: '#FFCB6B',
+    stringColor: '#c3e88d',
+    deletionColor: 'red',
+    sectionColor: '#FFCB6B',
+    commentColor: '#6b6b6b',
+    metaKeywordColor: 'red',
+    functionColor: '#82AAFF',
+    numberColor: '#ffcb6b'
+};
+
+export { api_url, theme };
