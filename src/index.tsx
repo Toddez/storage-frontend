66dabd0acda0088346d90658fb6aaaac5241acdf
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-13 15:33:13 +0100

    index

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import './style/index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>Hello world</div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
