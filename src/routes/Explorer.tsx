e279825a49e14d58421215d540093576a956dbdd
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-13 17:14:32 +0100

    Added unimplemented routes

---
+++ src/routes/Explorer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Explorer extends React.Component {
+    render() : JSX.Element {
+        return (
+            <div>EXPLORER ROUTE</div>
+        );
+    }
+}
+
+export default Explorer;
---
+++ src/routes/File.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class File extends React.Component {
+    render() : JSX.Element {
+        return (
+            <div>FILE ROUTE</div>
+        );
+    }
+}
+
+export default File;
