3bbe4ea5da46f0f0341ac8b9448f038216ffc27b
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-17 18:10:16 +0100

    Added inputs for selecting files

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import Modal from './Modal';
 
@@ -7,13 +7,25 @@ import '../style/Modal.css';
 class UploadModal extends Modal<ModalProps> {
     constructor(props: ModalProps) {
         super(props);
+
+        this.handleFileChange = this.handleFileChange.bind(this);
+    }
+
+    handleFileChange(event: ChangeEvent<HTMLInputElement>) : void {
+        this.setState({
+            data: {
+                ...this.state.data,
+                files: event.target.files
+            }
+        });
     }
 
     render() : JSX.Element {
         return (
             <div className='Modal' onClick={this.handleClick}>
                 <form className='modal-content' onSubmit={(data) => { this.handleSubmit(data); this.props.hide(); }}>
-                    Unimplemented Modal
+                    <input type="file" multiple name="file" id="file" onChange={this.handleFileChange} />
+                    <input type="submit" value="Upload"/>
                 </form>
             </div>
         );
