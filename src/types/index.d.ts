e2eaf5f9bc9a4b15f19d8bafe777f09fc5f85bd1
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-21 21:08:31 +0100

    Declared module and types for markdown syntax highlight

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+declare module 'react-syntax-highlighter/dist/esm/styles/prism/vs-dark' {
+    const style: any;
+    export default style;
+}
+
+declare type CodeRendererProps = {
+    language: string,
+    value: string
+}
+
+declare type ImageRendererProps = {
+    src: string,
+    alt: string
+}
