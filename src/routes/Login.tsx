1d891c7326991ada9e0485cbbb8d4dcd0c64df93
Teo Carlsson <carlsson.teo@gmail.com>
2021-01-22 19:04:49 +0100

    Added base support for 2fa

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import auth from '../models/auth';
+import Auth from '../models/auth';
 import { apiUrl } from '../models/config';
 
 import Form from '../components/Form';
@@ -11,45 +11,143 @@ interface Props {
     onLogin: () => void
 }
 
-class Login extends Form<Props> {
-    onSubmit(data: Record<string, unknown>) : void {
-        fetch(`${apiUrl}/authorize`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: data.id,
-                key: data.key
-            })
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                // TODO: display errors
-                if (res.errors)
-                    return;
+type State = {
+    data: Record<string, unknown | string | number | boolean>,
+    step: number,
+    config: string | null
+}
 
-                auth.setToken(res.data.token);
-                this.props.onLogin();
-            });
+class Login extends Form<Props> {
+    onSubmit(data: Record<string, string>) : void {
+        if (this.state.step === 0) {
+            fetch(`${apiUrl}/authorize`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: data.id,
+                    key: data.key
+                })
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    if (res.errors)
+                        return;
+
+                    Auth.setToken(res.data.token);
+                    this.setState({ step: 1 });
+
+                    fetch(`${apiUrl}/2faqrc`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json',
+                            'x-access-token': Auth.getToken()
+                        }
+                    })
+                        .then((res) => res.blob())
+                        .then((res) => {
+                            const urlCreator = window.URL || window.webkitURL;
+                            return urlCreator.createObjectURL(res);
+                        })
+                        .then((res) => {
+                            this.setState({ config: res });
+                        });
+                });
+        } else {
+            fetch(`${apiUrl}/2fa`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'x-access-token': Auth.getToken()
+                },
+                body: JSON.stringify({
+                    authKey: data.authKey0 + data.authKey1 + data.authKey2 + data.authKey3 + data.authKey4 + data.authKey5,
+                })
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    // TODO: display errors
+                    if (res.errors)
+                        return;
+
+                    Auth.setToken(res.data.token);
+                    this.props.onLogin();
+                });
+        }
+    }
+
+    state: State = {
+        data: {},
+        step: 0,
+        config: null
+    }
+
+    _isMounted = false;
+
+    componentDidMount() : void {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() : void {
+        this._isMounted = false;
+    }
+
+    onInput(event: any) : void {
+        if (event.target.value.length === 1) {
+            event.target.nextElementSibling.focus();
+        }
+    }
+
+    onKeydown(event: any) : void {
+        if (event.target.value.length === 0 && ["Backspace", "Delete"].includes(event.key)) {
+            event.target.previousElementSibling.focus();
+        }
     }
 
     render() : JSX.Element {
+        const authPattern = '[0-9]{1,1}';
+
         return (
             <div className='Login'>
-                <form onSubmit={this.handleSubmit} >
-                    <div className='input-section'>
-                        <input type='text' name='id' id='id' onChange={this.handleChange} required autoFocus />
-                        <label htmlFor='id'><span className='label-text'>ID</span></label>
-                    </div>
+                { this.state.step === 0 ?
+                    <form onSubmit={this.handleSubmit} >
+                        <div className='input-section'>
+                            <input key={0}  type='text' name='id' id='id' onChange={this.handleChange} required autoFocus />
+                            <label htmlFor='id'><span className='label-text'>ID</span></label>
+                        </div>
 
-                    <div className='input-section'>
-                        <input type='password' name='key' id='key' onChange={this.handleChange} required />
-                        <label htmlFor='key'><span className='label-text'>Key</span></label>
-                    </div>
+                        <div className='input-section'>
+                            <input key={1}  type='password' name='key' id='key' onChange={this.handleChange} required />
+                            <label htmlFor='key'><span className='label-text'>Key</span></label>
+                        </div>
 
-                    <input type='submit' value='Authorize' hidden />
-                </form>
+                        <input type='submit' value='Authorize' hidden />
+                    </form>
+                    :
+                    <form onSubmit={this.handleSubmit} >
+                        { this.state.config ?
+                            <div className='qr-code'>
+                                <img src={this.state.config} alt='qr code'/>
+                            </div>
+                            : null
+                        }
+
+                        <div className='input-section'>
+                            <div id='authKey' className='always-show'>
+                                <input key={0} className='authKey' type='number' name='authKey0' id='authKey0' onInput={this.onInput} onChange={this.handleChange} min={0} max={9} placeholder="." required autoFocus pattern={authPattern} />
+                                <input key={1} className='authKey' type='number' name='authKey1' id='authKey1' onKeyDown={this.onKeydown} onInput={this.onInput} onChange={this.handleChange} min={0} max={9} placeholder="." required pattern={authPattern} />
+                                <input key={2} className='authKey' type='number' name='authKey2' id='authKey2' onKeyDown={this.onKeydown} onInput={this.onInput} onChange={this.handleChange} min={0} max={9} placeholder="." required pattern={authPattern} />
+                                <input key={3} className='authKey' type='number' name='authKey3' id='authKey3' onKeyDown={this.onKeydown} onInput={this.onInput} onChange={this.handleChange} min={0} max={9} placeholder="." required pattern={authPattern} />
+                                <input key={4} className='authKey' type='number' name='authKey4' id='authKey4' onKeyDown={this.onKeydown} onInput={this.onInput} onChange={this.handleChange} min={0} max={9} placeholder="." required pattern={authPattern} />
+                                <input key={5} className='authKey' type='number' name='authKey5' id='authKey5' onKeyDown={this.onKeydown} onChange={this.handleChange} min={0} max={9} placeholder="." required pattern={authPattern} />
+                            </div>
+                            <label htmlFor='authKey'><span className='label-text'>2FA Key</span></label>
+                        </div>
+
+                        <input type='submit' value='Authorize' hidden />
+                    </form>
+                }
             </div>
         );
     }
